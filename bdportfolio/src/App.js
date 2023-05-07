d2c45f0852e32f72c88731a4db7014ed16177e69
bdiddy2022 <brendan.t.downs@gmail.com>
2023-05-07 16:14:35 -0400

    Photo Media

--- bdportfolio/src/App.js
+++ bdportfolio/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
-import { ThemeProvider, Typography, Button, Card, CardContent, Divider, Grid} from '@mui/material';
+import { Box, ThemeProvider, Typography, Button, Card, CardContent, Divider, Grid } from '@mui/material';
 import theme from './components/theme.js'
-import { SkillsBox, BackExperienceBox, PictureBox } from './components/layoutComponents';
+import { SkillsBox, BackExperienceBox } from './components/layoutComponents';
+import { PictureBox } from './components/pictureBox';
+
 
 function App() {
   return (
@@ -10,26 +12,27 @@ function App() {
         <Grid container>
           <Grid xs={12}>
             <Card size="xl" color="info" sx={{ width: '100%', marginBottom: 5, alignSelf: "center" }}>
-            <CardContent>
+              <CardContent>
                 <Typography variant='h1'>Brendan Downs</Typography >
                 <Divider variant='inset' sx={{ marginBottom: 2 }} />
                 <Button variant="outlined" color="secondary" >Get in Touch</Button>
                 <Button variant="outlined" color="primary">Portfolio</Button>
-            </CardContent>
+              </CardContent>
             </Card>
-            </Grid>
-          <Grid xs={1}/>
-          <Grid item spacing={3}>
+          </Grid>
+          <Grid xs={1} />
+          <Grid item spacing={4}>
             <SkillsBox />
           </Grid>
-          <Grid xs={4} >
+          <Grid xs={3}/>
+          <Grid item xs={4} sx={{alignContent: 'right'}}>
             <PictureBox/>
           </Grid>
-          <Grid xs={6} />
-          <Grid item spacing={3}>
+          <Grid xs={4} />
+          <Grid item spacing={3} xs={8}>
             <BackExperienceBox />
           </Grid>
-          <Grid xs={2}/>
+          <Grid xs={2} />
         </Grid>
       </div>
     </ThemeProvider>
--- bdportfolio/src/components/layoutComponents.js
+++ bdportfolio/src/components/layoutComponents.js
@@ -1,5 +1,4 @@
-import { Box, Typography, Divider, ImageList, ImageListItem } from "@mui/material";
-import './ProfessionalHeadshot.jpg';
+import { Box, Typography, Divider} from "@mui/material";
 
 const skillSet = ["Skillset", "I am a results-oriented junior web developer with an approach that oscillates somewhere between fascination and excitement. My base skillset is built on JavaScript (Angular), Java (Springboot/Thymeleaf), and SQL (MySQL), and has since expanded to include React and MongoDB"];
 const backgroundAndExperience = ["Background & Experience", `I studied Marine Engineering in college, and - after taking a hiatus to attend to my health - graduated with a bachelors degree in Facilities Engineering, my proudest achievement. I ran the gamut of engineering work, from shipboard generator swaps in Boston Harbor to working as a member of the design team for a multi-state project to bring high speed internet to underserved rural populations.`, `The skills that I developed during those years have turned out to be highly transferable to my new career path, and the persistence that I developed during my hiatus has been even more so`];
@@ -7,7 +6,7 @@ const backgroundAndExperience = ["Background & Experience", `I studied Marine En
 
 
 export const infoBox = (heading, info, boxWidth) => {
-    
+
     return (
         <Box sx={
             {
@@ -16,18 +15,18 @@ export const infoBox = (heading, info, boxWidth) => {
                 border: 4,
                 borderColor: '#ff4081',
                 backgroundColor: "primary.dark",
-                borderRadius:3,
+                borderRadius: 3,
                 padding: 2,
                 margin: 3,
                 textAlign: 'right'
 
             }
         }>
-            <Typography variant="h3" color="background.paper" sx={{ p: 1, backgroundColor: '#ff4081', borderRadius: 3, width: 'max'}}>{heading}</Typography>
-            <Divider sx={{padding: 1, marginBottom: 2 }}/>
-            
+            <Typography variant="h3" color="background.paper" sx={{ p: 1, backgroundColor: '#ff4081', borderRadius: 3, width: 'max' }}>{heading}</Typography>
+            <Divider sx={{ padding: 1, marginBottom: 2 }} />
+
             <Typography variant="h5" color="background.paper" >{info}</Typography>
-            
+
 
         </Box>
     );
@@ -38,27 +37,11 @@ export const SkillsBox = () => {
 }
 
 export const BackExperienceBox = () => {
-    return infoBox(backgroundAndExperience[0], backgroundAndExperience[1] + backgroundAndExperience[2], 550);
+    return infoBox(backgroundAndExperience[0], backgroundAndExperience[1] + backgroundAndExperience[2], 800);
 }
 
-export const PictureBox = () => {
-    return (
-        <Box>
-        <ImageList sx={{width: 500, height: 450}}>
-            <ImageListItem>
-                <img
-                    src='./ProfessionalHeadshot.jpg'
-                    alt='Headshot Portrait'
-                    loading="lazy"
-                />
-            </ImageListItem>
-        </ImageList>
-        </Box>
-    );
-}
 
 export default {
     SkillsBox,
     BackExperienceBox,
-    PictureBox
 };
